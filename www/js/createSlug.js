90e54bf624556d484c5ad331167d65d4690445a1
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-14 19:17:20 +0200

    live gerating posts slug

---
+++ www/js/createSlug.js
@@ -0,0 +1,23 @@
+var slug = function(str) {
+    str = str.replace(/^\s+|\s+$/g, ''); // trim
+    str = str.toLowerCase();
+
+    // remove accents, swap ñ for n, etc
+    var from = "ãàáäâẽèéëêìíïîõòóöôùúüûñç·/_,:;";
+    var to   = "aaaaaeeeeeiiiiooooouuuunc------";
+    for (var i=0, l=from.length ; i<l ; i++) {
+        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+    }
+
+    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+        .replace(/\s+/g, '-') // collapse whitespace and replace by -
+        .replace(/-+/g, '-'); // collapse dashes
+
+    return str;
+};
+
+$(document).ready(function (){
+    $('input[name="name"]').keyup(function (){
+        $('input[name="slug"]').val(slug($('input[name="name"]').val()));
+    });
+});
