8101a1138254bd301079c5bbdd191786250c62a5
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-17 08:46:02 +0200

    create new category compatibility

--- app/AdminModule/presenters/PostPresenter.php
+++ app/AdminModule/presenters/PostPresenter.php
@@ -124,48 +124,76 @@ class PostPresenter extends AdminPresenter
             $slug = $this->createSlug($values['slug']);
         }
 
-		if(count($values['tags'])){
-			$tags = array();
+		$tags = array();
+		if(isset($values['tags']) and is_array($values['tags']) and count($values['tags'])){
 			foreach($values['tags'] as $tag){
 				$tags[] = $this->tagFacade->getOne($tag);
 			}
 		}
 
-        if($this->id){
-            $this->post
-                ->setName($values['name'])
-                ->setSlug($values['slug'])
-                ->setDescription($values['description'])
-                ->setKeywords($values['keywords'])
-                ->setContent($values['content'])
-                ->setCategory($this->categoryFacade->getOne((int)$values['category']))
-                ->setTags($tags)
-                ->setPublish($values['publish'])
-                ->setComment($values['comment']);
+		if(isset($values['categoryNew']) and isset($values['category']) and !empty($values['categoryNew'])){
+			$category = $this->createNewCategory($values['categoryNew']);
+			if(!$category) $f->addError('Category with same name exist! Please try it again.');
 
-            $this->postFacade->persist($this->post);
-            $this->flashMessage('Post was edited');
-            $this->redirect('this');
+		}elseif(isset($values['categoryNew']) and !empty($values['categoryNew'])){
+			$category = $this->createNewCategory($values['categoryNew']);
+			if(!$category) $f->addError('Category with same name exist! Please try it again.');
 
-        }else{
-            $post = new \Flame\Models\Posts\Post(
-                $this->userFacade->getOne($this->getUser()->getId()),
-                $values['name'],
-                $slug,
-                $values['description'],
-                $values['keywords'],
-                $values['content'],
-	            $this->categoryFacade->getOne($values['category']),
-	            $tags,
-                $values['publish'],
-                $values['comment']
-            );
+		}elseif(isset($values['category'])){
+			$category = $values['category'];
 
-            $this->postFacade->persist($post);
-            $this->flashMessage('Post was successfully added.', 'success');
-            $this->redirect('Post:');
-        }
+		}else{
+			$f->addError('Category is required. Please select one.');
 
+		}
+
+		if(!$f->hasErrors()){
+	        if($this->id){
+	            $this->post
+	                ->setName($values['name'])
+	                ->setSlug($values['slug'])
+	                ->setDescription($values['description'])
+	                ->setKeywords($values['keywords'])
+	                ->setContent($values['content'])
+	                ->setCategory($this->categoryFacade->getOne($category))
+	                ->setTags($tags)
+	                ->setPublish($values['publish'])
+	                ->setComment($values['comment']);
+		        //dump($this->post);exit;
+	            $this->postFacade->persist($this->post);
+		        $this->flashMessage('Post was edited');
+		        $this->redirect('this');
+
+	        }else{
+	            $post = new \Flame\Models\Posts\Post(
+	                $this->userFacade->getOne($this->getUser()->getId()),
+	                $values['name'],
+	                $slug,
+	                $values['description'],
+	                $values['keywords'],
+	                $values['content'],
+		            $this->categoryFacade->getOne($category),
+		            $tags,
+	                $values['publish'],
+	                $values['comment']
+	            );
+
+	            $this->postFacade->persist($post);
+		        $this->flashMessage('Post was successfully added.', 'success');
+		        $this->redirect('Post:');
+
+	        }
+		}
+
+	}
+
+	private function createNewCategory($name)
+	{
+		if($this->categoryFacade->getOneByName($name)) return false;
+
+		$category = new \Flame\Models\Categories\Category($name, "", $this->createSlug($name));
+		$this->categoryFacade->persist($category);
+		return $category->getId();
 	}
 
 }
