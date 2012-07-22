4158c82abdb62c37dc89d5d1b9361f80e0f47681
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-22 13:44:59 +0200

    starting the app

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -8,16 +8,31 @@
  * @date    14.07.12
  */
 
-use Nette\Application\Routers\Route,
+use Flame\Config\Configurator,
+	Nette\Application\Routers\Route,
 	Nette\Application\Routers\RouteList;
 
 require LIBS_DIR . '/autoload.php';
 //Is not required include nette and flame loader because it do the composer!
 
+if(!defined('NETTE')) die ('You must load Nette loader first');
+
+$configurator = new Configurator();
+$configurator->setOptionalParameters();
+$configurator->enableDebugger(WWW_DIR . '/log');
+$configurator->setTempDirectory(WWW_DIR . '/temp');
+$configurator->createRobotLoader()->addDirectory(APP_DIR)->register();
+$configurator->addConfig(FLAME_DIR . '/Config/config.neon');
+$configurator->setDatabaseParametersFromEnv();
+$container = $configurator->createContainer();
+
+$doctrineConfig = $container->getService('EntityManagerConfig');
+$doctrineConfig->setSQLLogger(\Flame\Utils\ConnectionPanel::register());
+
 $container->router[] = new Route('index.php', 'Front:Homepage:default', Route::ONE_WAY);
 
 $container->router[] = $adminRouter = new RouteList('Admin');
-	$adminRouter[] = new Route('admin/<presenter>/<action>[/<id>]', 'Dashboard:default');
+$adminRouter[] = new Route('admin/<presenter>/<action>[/<id>]', 'Dashboard:default');
 
 $container->router[] = $frontRouter = new RouteList('Front');
-	$frontRouter[] = new Route('<presenter>/<action>[/<id>][/<slug>]', 'Homepage:default');
+$frontRouter[] = new Route('<presenter>/<action>[/<id>][/<slug>]', 'Homepage:default');
