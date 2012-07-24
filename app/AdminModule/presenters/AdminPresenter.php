8f59abe51383af712cdaa5f4e29f3bc4fc69e57d
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-24 20:47:34 +0200

    autogeneratign main menu for users with another roles

--- app/AdminModule/presenters/AdminPresenter.php
+++ app/AdminModule/presenters/AdminPresenter.php
@@ -21,6 +21,12 @@ abstract class AdminPresenter extends \Flame\Application\UI\Presenter
 		}
 	}
 
+	public function beforeRender()
+	{
+		parent::beforeRender();
+		$this->template->menuItems = $this->generateMainMenu();
+	}
+
 	public function handleSignOut()
 	{
 		$this->getUser()->logout();
@@ -38,5 +44,43 @@ abstract class AdminPresenter extends \Flame\Application\UI\Presenter
 		$url = preg_replace('~[^-a-z0-9_]+~', '', $url);
 
 		return $url;
+	}
+
+	private function generateMainMenu()
+	{
+		$items = array(
+			array('page' => 'Dashboard:', 'title' => 'Dashboard'),
+			array('page' => 'Post:', 'title' => 'Posts'),
+			array('page' => 'Category:', 'title' => 'Categories'),
+			array('page' => 'Tag:', 'title' => 'Tags'),
+			array('page' => 'Page:', 'title' => 'Pages'),
+			array('page' => 'Image:', 'title' => 'Images'),
+			array('page' => 'Comment:', 'title' => 'Comments'),
+			array('page' => 'Newsreel:', 'title' => 'Newsreel'),
+			array('page' => 'Option:', 'title' => 'Options'),
+			array('page' => 'User:', 'title' => 'Users'),
+			array('page' => 'Import:', 'title' => 'Import posts'),
+		);
+
+		$menu = array();
+
+		foreach($items as $item){
+			if(isset($item['page']) and isset($item['title'])){
+				$parts = explode(':', $item['page']);
+
+				$user = $this->getUser();
+				if(count($parts) <= 2){
+					if($user->isAllowed('Admin:' . $parts[0], $parts['1'])){
+						$menu[] = (object) $item;
+					}
+				}else{
+					if($user->isAllowed( $parts[0] . ':' . $parts[1], $parts['2'])){
+						$menu[] = (object) $item;
+					}
+				}
+			}
+		}
+
+		return $menu;
 	}
 }
