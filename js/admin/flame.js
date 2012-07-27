c4a02f734a1f30b1c5ab9e940d88110e3a1e0517
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-07-27 23:35:16 +0200

    switch between markdown and mceeditor is not required globally

--- js/admin/flame.js
+++ js/admin/flame.js
@@ -1,11 +1,4 @@
 
 $(document).ready(function (){
-    $('.use-markdown-syntax').change(function (){
-        if($('.use-markdown-syntax').attr('checked')){
-            tinyMCE.execCommand('mceFocus', false, 'frm-postForm-content');
-            tinyMCE.execCommand('mceRemoveControl', false, 'frm-postForm-content');
-        }else{
-            tinyMCE.execCommand('mceAddControl', false, 'frm-postForm-content');
-        }
-    });
+
 });
