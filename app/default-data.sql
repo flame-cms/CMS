e08aa050d089f5687bbb77a236f61502e5b0aaf6
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-08-04 11:22:48 +0200

    used crypt

--- app/default-data.sql
+++ app/default-data.sql
@@ -1,2 +1,2 @@
-INSERT INTO `users` (`id`, `info_id`, `email`, `password`, `role`, `facebook`) VALUES
-(1,	NULL,	'user@demo.com',	'552dc2e616c351e1a6ffaadb32dbacbaaeeb8359a9f6ec33668e9265997c8aa8fa8b501c6759b989742bf0b4e566ecf2079f9359d3224ecef116ce42c4ec07ad',	'administrator',	NULL);
+INSERT INTO `users` (`id`, `info_id`, `password`, `role`, `email`, `facebook`) VALUES
+(1,	NULL,	'$2a$07$8237dee42f4370e9015dbOkJ.dMdxysALY0pC2YtQLfc8NwppXM/i',	'administrator',	'user@demo.com',	NULL);
