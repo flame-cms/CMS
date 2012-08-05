7d4f272f8739fb2f8f4c0c4589bd3eb0f2ce9e72
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-08-05 18:27:07 +0200

    added tests of sign and Dashboard presenters, removed test of Homepage

---
+++ tests/case/selenium/SingPresenterTest.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ * SingPresenterTest.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame\CMS
+ *
+ * @date    05.08.12
+ */
+
+namespace Flame\CMS\Tests\Selenium;
+
+class SingPresenterTest extends \Flame\CMS\Tests\SeleniumTestCase
+{
+	/**
+	 * @test
+	 */
+	public function testEmptyForm()
+	{
+		$this->url('/admin/sign/in');
+		$this->clickOnElement('frm-signInForm-login');
+		$this->assertEquals('Field "Email:" is required.', $this->alertText());
+	}
+
+	/**
+	 * @test
+	 */
+	public function testNoPassword()
+	{
+		$this->url('/admin/sign/in');
+		$element = $this->byId('frm-signInForm-email');
+		$element->value('user@demo.com');
+		$this->clickOnElement('frm-signInForm-login');
+		$this->assertEquals('Field "Password:" is required.', $this->alertText());
+	}
+
+	/**
+	 * @test
+	 */
+	public function testNoEmail()
+	{
+		$this->url('/admin/sign/in');
+		$element = $this->byId('frm-signInForm-password');
+		$element->value('password12');
+		$this->clickOnElement('frm-signInForm-login');
+		$this->assertEquals('Field "Email:" is required.', $this->alertText());
+	}
+
+	/**
+	 * @test
+	 */
+	public function testLogin()
+	{
+		$this->url('/admin/sign/in');
+
+		$element = $this->byId('frm-signInForm-email');
+		$element->value('user@demo.com');
+
+		$element = $this->byId('frm-signInForm-password');
+		$element->value('password12');
+
+		$this->clickOnElement('frm-signInForm-login');
+
+		$this->assertEquals($this->trimUrl($this->url()), 'admin');
+	}
+
+}
