174b88d5cdf58d40064f262316ba78f47c6ae7cd
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-08-06 22:27:42 +0200

    added tests

---
+++ tests/case/selenium/PostPresenterTest.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * PostPresenterTest.php
+ *
+ * @author  Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @package Flame\CMS
+ *
+ * @date    06.08.12
+ */
+
+namespace Flame\CMS\Tests\Selenium;
+
+class PostPresenterTest extends \Flame\CMS\Tests\SeleniumTestCase
+{
+	
+	/**
+	 * @test
+	 */
+	public function testAddEmptyContent()
+	{
+		$this->loginUser();
+		$this->url('/admin/post/add');
+
+		$name = $this->byName('name');
+		$name->value('name');
+
+//		$this->frame('frm-postForm-content_ifr');
+//		$content = $this->byId('tinymce');
+//		$content->value('content');
+
+		$category = $this->byName('categoryNew');
+		$category->value('category');
+
+		$this->clickOnElement('frm-postForm-send');
+
+		//$error = $this->byCssSelector('h4');
+		//$this->assertEquals('Post was successfully added.', $error->text());
+		$this->assertEquals('Field "Content:" is required.', $this->alertText());
+	}
+
+}
