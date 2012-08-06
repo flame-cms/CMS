beb409b2e4be1325b272a5978680028dd5b6d52d
Jiří Šifalda <sifalda.jiri@gmail.com>
2012-08-06 14:50:15 +0200

    added constants to config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -20,6 +20,7 @@ $configurator->enableDebugger(WWW_DIR . '/../log');
 $configurator->setTempDirectory(WWW_DIR . '/../temp');
 $configurator->createRobotLoader()->addDirectory(APP_DIR)->register();
 $configurator->addConfig(APP_DIR . '/Config/config.neon', $configurator::AUTO);
+$configurator->addParameters(array('appDir' => APP_DIR, 'wwwDir' => WWW_DIR));
 $container = $configurator->createContainer();
 
 if ($container->parameters['consoleMode']) {
