d41d53af0b8a178c611e4f3530317d55c77142cb
JSifalda <sifalda.jiri@gmail.com>
2012-08-11 13:40:08 +0200

    removed white space

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,7 +4,6 @@ define('WWW_DIR', __DIR__ . '/../www');
 define('APP_DIR', WWW_DIR . '/../app');
 define('LIBS_DIR', WWW_DIR . '/../libs');
 
-
 require APP_DIR . '/bootstrap.php';
 
 require __DIR__ . '/UnitTestCase.php';
