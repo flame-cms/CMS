7357f193832ced08741522d6636a9ff3fef4d101
JSifalda <sifalda.jiri@gmail.com>
2012-08-12 19:35:32 +0200

    used ternary operator

--- app/Config/production.php
+++ app/Config/production.php
@@ -8,20 +8,14 @@
  * @date    28.07.12
  */
 
-$config = array();
-
-if(isset($_SERVER['DB1_HOST'], $_SERVER['DB1_NAME'], $_SERVER['DB1_USER'], $_SERVER['DB1_PASS'], $_SERVER['DB1_PORT'])){
-	$config = array(
-		'parameters' => array(
-			'database' => array(
-				'host' => $_SERVER['DB1_HOST'],
-				'dbname' => $_SERVER['DB1_NAME'],
-				'user' => $_SERVER['DB1_USER'],
-				'password' => $_SERVER['DB1_PASS'],
-				'port' => $_SERVER['DB1_PORT'],
-			),
+return array(
+	'parameters' => array(
+		'database' => array(
+			'host' => (isset($_SERVER['DB1_HOST'])) ? $_SERVER['DB1_HOST'] : '',
+			'dbname' => (isset($_SERVER['DB1_NAME'])) ? $_SERVER['DB1_NAME'] : '',
+			'user' => (isset($_SERVER['DB1_USER'])) ? $_SERVER['DB1_USER'] : '',
+			'password' => (isset($_SERVER['DB1_PASS'])) ? $_SERVER['DB1_PASS'] : '',
+			'port' => (isset($_SERVER['DB1_PORT'])) ? $_SERVER['DB1_PORT'] : 3306,
 		),
-	);
-}
-
-return $config;
+	),
+);
