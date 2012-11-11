d2b287d2ac6b7f2c8dbef2a6407f6cf9db6073b6
JSifalda <sifalda.jiri@gmail.com>
2012-11-11 21:21:11 +0100

    Add advanced Datepicker

--- app/Application/UI/Form.php
+++ app/Application/UI/Form.php
@@ -15,9 +15,10 @@ abstract class Form extends \Flame\Application\UI\Form
 
 	public function __construct()
 	{
-		parent::__construct(null, null);
+		parent::__construct();
 
 		$this->setRenderer(new \Kdyby\Extension\Forms\BootstrapRenderer\BootstrapRenderer);
+		\Flame\Forms\Controls\BootstrapDatePicker::register();
 	}
 
 }
