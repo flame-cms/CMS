9f664f1776a7de2d988bf081e20468a0de4c92db
JSifalda <sifalda.jiri@gmail.com>
2013-01-10 22:12:47 +0100

    Refactoring

--- AdminModule/presenters/AdminPresenter.php
+++ AdminModule/presenters/AdminPresenter.php
@@ -54,52 +54,69 @@ abstract class AdminPresenter extends \Flame\Application\UI\SecuredPresenter
 	protected function createComponentNavbarBuilder()
 	{
 		$control = $this->navbarBuilderControlFactory->create();
-		$control->setTitle('Dashboard', 'Dashboard:');
+		$control->setTitle('Administration', 'Dashboard:');
 		$control->displayUserbar();
 
 		$navbar = $control->getNavbarControl();
 
-		$items = array(
-			array('Posts', 'Post:'),
-			array('List', 'Post:', 'Posts'),
-			array('Add', 'Post:add', 'Posts'),
-			array('Import', 'Import:', 'Posts', true),
-			array('Categories', 'Category:', 'Posts', true),
-			array('Tags', 'Tag:', 'Posts'),
-			array('Comments', 'Comment:', 'Posts'),
-
-			array('Newsreel', 'Newsreel:'),
-			array('Images', 'Image:'),
-			array('List', 'Image:', 'Images'),
-			array('Upload', 'Image:upload', 'Images'),
-			array('Category', 'ImageCategory:', 'Images'),
-			array('Pages', 'Page:'),
-			array('Links', 'Link:'),
-
-			array('Setting'),
-			array('Options', 'Option:', 'Setting'),
-			array('Users', 'User:', 'Setting'),
-			array('Menu', 'Menu:', 'Setting'),
-
-		);
+		$items = $this->getNavItems();
 
 		foreach($items as $item){
-			if(isset($item[1])){
-				$linkParts = explode(':', $item[1]);
+			if(!isset($item['label'])) continue;
+			if(isset($item['link'])){
+				$linkParts = explode(':', $item['link']);
+				if(count($linkParts) < 2) continue;
 
 				if($this->getUser()->isAllowed('Admin:' . $linkParts[0], $linkParts[1])){
-					$navbar->addNavbarItem($item[0], $this->link($item[1]), (isset($item[2])) ? $item[2] : null, (isset($item[3])) ? $item[3] : false);
+					$navbar->addNavbarItem(
+						$item['label'],
+						$this->link($item['link']),
+						(isset($item['parent'])) ? $item['parent'] : null,
+						(isset($item['separated'])) ? $item['separated'] : false,
+						(isset($item['icon'])) ? $item['icon'] : null
+					);
 				}
 			}else{
-				$navbar->addNavbarItem($item[0], '.', null, false);
+				$navbar->addNavbarItem($item['label'], '.', null, false, (isset($item['icon'])) ? $item['icon'] : null);
 			}
 		}
 
 		$userbar = $control->getUserbarControl();
-		$userbar->addItem('Account settings', $this->link('User:edit'));
-		$userbar->addItem('Change password', $this->link('User:password'));
+		$userbar->addItem('Account settings', $this->link('User:edit'), 'icon-edit');
+		$userbar->addItem('Change password', $this->link('User:password'), 'icon-lock');
 		$userbar->setUserName($this->getUser()->getIdentity());
 
 		return $control;
+	}
+
+	/**
+	 * @return array
+	 */
+	protected function getNavItems()
+	{
+		return array(
+			array('label' => 'Posts'),
+			array('label' => 'List', 'link' => 'Post:', 'parent' => 'Posts'),
+			array('label' => 'Add', 'link' => 'Post:add', 'parent' => 'Posts'),
+			array('label' => 'Import', 'link' => 'Import:', 'parent' => 'Posts', 'separated' => true),
+			array('label' => 'Categories', 'link' => 'Category:', 'parent' => 'Posts','separated' =>  true),
+			array('label' => 'Tags', 'link' => 'Tag:', 'parent' => 'Posts'),
+			array('label' => 'Comments', 'link' => 'Comment:', 'parent' => 'Posts'),
+
+			array('label' => 'Newsreel', 'link' => 'Newsreel:'),
+			array('label' => 'Pages', 'link' => 'Page:'),
+			array('label' => 'Links', 'link' => 'Link:'),
+
+			array('label' => 'Images'),
+			array('label' => 'List', 'link' => 'Image:', 'parent' => 'Images'),
+			array('label' => 'Upload', 'link' => 'Image:upload', 'parent' => 'Images'),
+			array('label' => 'Category', 'link' => 'ImageCategory:', 'parent' => 'Images'),
+
+			array('label' => 'Setting'),
+			array('label' => 'Options', 'link' => 'Option:', 'parent' => 'Setting'),
+			array('label' => 'Users', 'link' => 'User:', 'parent' => 'Setting'),
+			array('label' => 'Menu', 'link' => 'Menu:', 'parent' => 'Setting'),
+
+		);
 	}
 }
